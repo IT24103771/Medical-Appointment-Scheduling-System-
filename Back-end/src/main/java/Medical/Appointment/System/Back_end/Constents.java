01a60dd4eefe06e133e7b99783fab28a70387063
IT24103771 <IT24103771@my.sliit.lk>
2025-05-14 10:48:29 +0530

    Add files via upload

---
+++ Back-end/src/main/java/Medical/Appointment/System/Back_end/Constents.java
@@ -0,0 +1,6 @@
+package Medical.Appointment.System.Back_end;
+
+public class Constents {
+    public final String usersListFilePath = "src/main/java/Medical/Appointment/System/Back_end/UserRegistration/Registration.txt";
+    public final String appointmentFilePath = "src/main/java/Medical/Appointment/System/Back_end/ApointmentSytem/appointments.txt";
+}
